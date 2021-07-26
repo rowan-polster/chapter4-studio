9a03c7e7a2750bd3ef9b3bebede1412487d7cc3a
Rowan Polster <rowandownriver94@gmail.com>
2021-07-26 18:29:52 -0500

    made Menu its own class

--- chapter4-studio/Menu.cs
+++ chapter4-studio/Menu.cs
@@ -4,25 +4,10 @@ using System.Text;
 
 namespace chapter4_studio
 {
-    public class Menu
+    class Menu
     {
-        private DateTime lastUpdated {get; set;}
-        public class MenuItems
-        {
-            private string Name { get; set; }
-            private double Price { get; set; }
-            private string Description { get; set; }
-            private List<string> Category { get; set; }
-            private bool IsNew { get; set; }
+        private string MenuItem { get; set; }
+        private DateTime LastUpdated { get; set; }
 
-            public MenuItems(string name, double price, string description, List<string> category, bool isNew)
-            {
-                this.Name = name;
-                this.Price = price;
-                this.Description = description;
-                this.Category = category;
-                this.IsNew = isNew;
-            }
-        }
     }
 }
---
+++ chapter4-studio/MenuItems.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace chapter4_studio
+{
+        public class MenuItems
+        {
+            private string Name { get; set; }
+            private double Price { get; set; }
+            private string Description { get; set; }
+            private List<string> Category { get; set; }
+            private bool IsNew { get; set; }
+
+            public MenuItems(string name, double price, string description, List<string> category, bool isNew)
+            {
+                this.Name = name;
+                this.Price = price;
+                this.Description = description;
+                this.Category = category;
+                this.IsNew = isNew;
+            }
+        }
+    }
+
